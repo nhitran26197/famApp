e9beeee5dfff287bee41d7a166d2925df9830ad5
Saja Hussein <106775554+Iremiaa@users.noreply.github.com>
2023-04-19 04:52:13 -0500

    feedpage

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import "./Styles/App.css";
+import Navbar from "./Components/NavBar.js";
+import Post from "./Components/Post.js";
+import AddButton from "./Components/AddButton.js";
+
+function App() {
+  return (
+    <div classsName="container">
+      <div>
+        <Navbar />
+      </div>
+      <div className="content">
+        <div className="feed">
+          <div>
+            <Post />
+            <Post />
+            <div className="h-24" />
+          </div>
+          <div>
+            <AddButton />
+          </div>
+        </div>
+        <div>google maps</div>
+      </div>
+    </div>
+  );
+}
+export default App;
---
+++ frontend/src/Components/AddButton.js
@@ -0,0 +1,13 @@
+import "../Styles/App.css";
+
+function AddButton() {
+  return (
+    <div className="add-post-button">
+      <button className="button">
+        <span className="circle">+</span>
+        Add a post
+      </button>
+    </div>
+  );
+}
+export default AddButton;
