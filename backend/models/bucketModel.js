82ae42210ccbb1a6b237e8bfc7abe3f20c2ac31e
Parth Sharma <pxs4528@mavs.uta.edu>
2023-04-21 23:18:23 -0500

    bucket list model

---
+++ backend/models/bucketModel.js
@@ -0,0 +1,12 @@
+//bucket list model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bucketSchema = new Schema({
+    title: String,
+    description: String,
+    location: String,
+});
+
+const Bucket = mongoose.model('Bucket', bucketSchema);
+module.exports = Bucket;
