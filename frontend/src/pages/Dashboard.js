45cc6769e3accdf89ce65e3caf4196336c5ac65e
Chandrashekar Dasari <chandra.dasari.cs@gmail.com>
2023-04-25 00:03:53 -0500

    button styling

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -49,19 +49,33 @@ export default function Dashboard() {
               <div className="flex flex-row justify-center">
                 <button
                   type="button"
-                  className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                  style={{ backgroundColor: "#9EBC9E" }}
+                  className=" ml-4 flex whitespace-nowrap text-black bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   onClick={() => setOpen(true)}
                   ref={cancelButtonRef}
                 >
                   add member
+                  <svg width={"25px"} className="ml-2 mb-1" class="svg-icon" viewBox="0 0 20 20">
+                    <path d="M12.075,10.812c1.358-0.853,2.242-2.507,2.242-4.037c0-2.181-1.795-4.618-4.198-4.618S5.921,4.594,5.921,6.775c0,1.53,0.884,3.185,2.242,4.037c-3.222,0.865-5.6,3.807-5.6,7.298c0,0.23,0.189,0.42,0.42,0.42h14.273c0.23,0,0.42-0.189,0.42-0.42C17.676,14.619,15.297,11.677,12.075,10.812 M6.761,6.775c0-2.162,1.773-3.778,3.358-3.778s3.359,1.616,3.359,3.778c0,2.162-1.774,3.778-3.359,3.778S6.761,8.937,6.761,6.775 M3.415,17.69c0.218-3.51,3.142-6.297,6.704-6.297c3.562,0,6.486,2.787,6.705,6.297H3.415z"></path>
+                  </svg>
                 </button>
                 <button
                   type="button"
-                  className="mt-3 ml-4 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                  style={{ backgroundColor: "#9EBC9E" }}
+                  className=" ml-4 flex whitespace-nowrap text-black bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   onClick={() => setOpenPost(true)}
                   ref={cancelButtonRef}
                 >
-                  add Post
+                  <span>add Post</span>
+                  <svg
+                    width={"25px"}
+                    className="ml-2 mb-1"
+                    class="svg-icon"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path d="M18.303,4.742l-1.454-1.455c-0.171-0.171-0.475-0.171-0.646,0l-3.061,3.064H2.019c-0.251,0-0.457,0.205-0.457,0.456v9.578c0,0.251,0.206,0.456,0.457,0.456h13.683c0.252,0,0.457-0.205,0.457-0.456V7.533l2.144-2.146C18.481,5.208,18.483,4.917,18.303,4.742 M15.258,15.929H2.476V7.263h9.754L9.695,9.792c-0.057,0.057-0.101,0.13-0.119,0.212L9.18,11.36h-3.98c-0.251,0-0.457,0.205-0.457,0.456c0,0.253,0.205,0.456,0.457,0.456h4.336c0.023,0,0.899,0.02,1.498-0.127c0.312-0.077,0.55-0.137,0.55-0.137c0.08-0.018,0.155-0.059,0.212-0.118l3.463-3.443V15.929z M11.241,11.156l-1.078,0.267l0.267-1.076l6.097-6.091l0.808,0.808L11.241,11.156z"></path>
+                  </svg>
                 </button>
               </div>
             </div>
@@ -170,7 +184,7 @@ export default function Dashboard() {
             </Transition.Root>
           </div>
         </div>
-        <div>
+        <div className="mt-6">
           <Wrapper
             apiKey={"AIzaSyCecWQ01w2hL3sX2XNdjBVsL0KA9Yln4Hs"}
             version="beta"
