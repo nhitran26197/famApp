1f9ecd452fe0b5e0bf831c4f4ee3cb4b091cd4e7
Parth <pxs4528@mavs.uta.edu>
2023-04-26 00:57:34 -0500

    creates and pulls bucket list

--- frontend/src/components/addGoal.js
+++ frontend/src/components/addGoal.js
@@ -1,13 +1,62 @@
 import "../css/Profile.css";
 import React from "react";
+import axios from "axios";
 
 function AddGoal(props) {
-  return (
-    <button className="button text-blueGray-700" onClick={props.onClick}>
+  const dialogRef = React.useRef(null);
+  const [title, setTitle] = React.useState("");
+  const [description, setDescription] = React.useState("");
+  const [location, setLocation] = React.useState("");
+
+  return (<>
+    <button className="button text-blueGray-700" onClick={() => {
+      dialogRef.current?.showModal();
+    }}>
       {props.label}
       Add Goal
     </button>
-  );
+
+    <dialog ref={dialogRef}>
+      <form class="flex flex-col gap-4">
+        <h2>Add Goal</h2>
+        <label>
+          Title:{" "}
+          <input type="text" name="name" onChange={
+            (e) => setTitle(e.target.value)
+          }  />
+        </label>
+        <label>
+          Description:{" "}
+          <input type="text" name="name"  onChange={(e) => setDescription(e.target.value)}/>
+        </label>
+        <label>
+          Location:{" "}
+          <input type="text" name="name" onChange={(e) => setLocation(e.target.value)}/>
+        </label>
+        <button type="button" onClick={async (e) => {
+          e.preventDefault();
+          // await fetch("/api/goals", {
+          alert('Submit your mom')
+          dialogRef.current?.close();
+
+          axios
+            .post("http://localhost:3030/BucketList/create", {
+              title,
+              description,
+              location,
+            })
+            .then((res) => {
+              console.log(res);
+              console.log(res.data);
+            }
+            )
+
+
+        }}>Submit</button>
+      </form>
+
+    </dialog>
+  </>);
 }
 
 export default AddGoal;
