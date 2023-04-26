f0feeb0180cb390f9533ed3c962f60d30cc5f762
Chandrashekar Dasari <chandra.dasari.cs@gmail.com>
2023-04-25 21:13:41 -0500

    member id sync issue fixed

--- backend/controllers/signup.js
+++ backend/controllers/signup.js
@@ -1,89 +1,81 @@
-const Member = require('../models/member');
-const User = require('../models/user');
-const mongoose = require('mongoose');
-
+const Member = require("../models/member");
+const User = require("../models/user");
+const mongoose = require("mongoose");
 
 const signUp = async (req, res) => {
-    //user input
-    const { username, password, email, firstName, lastName, age } = req.body;
-    console.log(username);
-    const bucket_list = "";
-    const post = [];
+  //user input
+  const { username, password, email, firstName, lastName, age } = req.body;
+  console.log(username);
+  const bucket_list = "";
+  const post = [];
 
-    let exitCondition = 0;
-
-    let estimate = await User.estimatedDocumentCount();
-    const new_User = new User({
-        username: username,
-        password: password,
-        email: email,
-        bucket_list: bucket_list,
-        post: post,
-        member_id: estimate,
-      });
-
-    const new_Member = new Member({
-        member_id: estimate,
-        name: firstName + " " + lastName,
-        age: age,
-        picture: "https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg",
-        account: username,
-        parent: [],
-        children: [],
-        sibling: [],
-        spouse: [],
+  let member_id;
+  let is_exist = false;
+  do {
+    member_id = Math.floor(Math.random() * 1000); // a random number from 0 to 999
+    Member.findOne({ member_id: member_id }).then((check) => {
+      if (check != null) {
+        console.log("member_id already exits");
+        is_exist = true;
+      }
     });
+  } while (is_exist);
 
-    console.log(new_User);
+  let exitCondition = 0;
 
-    User.exists({username: username}, (err, result) => {
-        if(err) {
-            console.log(err);
-            res.status(500).send();
-            console.log("no?");
-        }
-        else {
-            console.log(result);
-            console.log("yes?");
-        }
-    })
+  const new_User = new User({
+    username: username,
+    password: password,
+    email: email,
+    bucket_list: bucket_list,
+    post: post,
+    member_id: member_id,
+  });
 
+  const new_Member = new Member({
+    member_id: member_id,
+    name: firstName + " " + lastName,
+    age: age,
+    picture:
+      "https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg",
+    account: username,
+    parent: [],
+    children: [],
+    sibling: [],
+    spouse: [],
+  });
 
-    while(User.exists({member_id: estimate}))
-    {
-        console.log("member id already exists so adding one");
-        estimate = estimate + 1;
-        exitCondition = exitCondition + 1;
-        if(exitCondition > 10)
-        {
-            console.log("leave me alone");
-            break;
-        }
+  console.log(new_User);
+
+  User.exists({ username: username }, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+      console.log("no?");
+    } else {
+      console.log(result);
+      console.log("yes?");
     }
-    new_User.member_id = estimate;
-    console.log("ITS FINALLY WORKING");
-    
+  });
 
-    User.create(new_User, (err, user) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send();
-        }
-        else {
-            res.status(200).send();
-        }
-    });
+  User.create(new_User, (err, user) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+    } else {
+      res.status(200).send();
+    }
+  });
 
-    Member.create(new_Member, (err, member) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send();
-        }
-        else {
-            res.status(200).send();
-        }
-    });
-    console.log("It made it all the way cuhhhh");
-}
+  Member.create(new_Member, (err, member) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+    } else {
+      res.status(200).send();
+    }
+  });
+  console.log("It made it all the way cuhhhh");
+};
 
 module.exports = signUp;
